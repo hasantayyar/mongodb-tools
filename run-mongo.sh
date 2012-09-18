235b32e2e4597261efee91c1b92588290239460c
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2012-09-18 13:54:54 +0300

    run mongo sh

--- run-mongo.sh
+++ run-mongo.sh
@@ -4,6 +4,10 @@ if [ "MONGODB_INSTALLED" == "" ]; then
     echo -e "mongodb NOT found."
     exit 1
 fi
-
-mkdir -p ./.data
-mongod --dbpath ./.data
+if [ -z $1 ]
+then
+	echo "give data folder path"
+	exit
+fi
+sudo mkdir -p $1
+sudo mongod --dbpath $1
